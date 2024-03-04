0aa56008cfbc35e4a802fd9e2d9e07a29d603520
Marco12334 <150499999+Marco12334@users.noreply.github.com>
2024-03-03 23:27:45 -0500

    Update AsteriskPattern10#.java

--- AsteriskPattern10#.java
+++ AsteriskPattern10#.java
@@ -4,7 +4,7 @@ public class AsteriskPattern {
             for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
-            System.out.println(); // Move to the next line after printing each row of stars
+            System.out.println();
         }
     }
 }
